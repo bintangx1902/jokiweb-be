5358edeede0599814795e2eed71f5ead9d340436
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-06-11 17:35:03 +0700

    add random string generator

--- sertif/utils.py
+++ sertif/utils.py
@@ -1,3 +1,6 @@
+import string, random
+
+
 def get_template(temp: str):
     return f"certif/{temp}.html"
 
@@ -5,3 +8,10 @@ def get_template(temp: str):
 def find_data(model, nim, type):
     data = model.objects.filter(nim=nim, type=type)
     return True if data else False
+
+
+def create_slug(n):
+    letters = string.ascii_letters
+    num = '1234567890'
+    raw = letters + num
+    return ''.join(random.sample(raw, n))
